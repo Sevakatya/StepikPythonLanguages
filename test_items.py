e670f3ad99eaac539479544145d4b1a74d7620f3
Sevakatya <sevakatya@gmail.com>
2020-03-19 22:50:55 +0200

    Homework 3_6 autotest for different languages

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+class TestLanguages():
+    def test_language_on_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        browser.get(link)
+        time.sleep(30)
+        checkbasket = browser.find_element_by_css_selector("#add_to_basket_form")
+        assert checkbasket.is_displayed()
