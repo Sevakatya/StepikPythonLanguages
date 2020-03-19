5e78f8fc4e8b482e1eb527cfb37c6c7fa008b7af
Sevakatya <sevakatya@gmail.com>
2020-03-19 23:05:56 +0200

    Homework 3_6 autotest for different languages

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 import time
 class TestLanguages():
-    def test_language_on_page(self, browser):
+    def test_different_languages(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
         time.sleep(30)
