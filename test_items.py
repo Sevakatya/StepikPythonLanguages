35554c111c1b37535c21bfff9dd1c285f5605c18
Sevakatya <sevakatya@gmail.com>
2020-03-20 18:27:19 +0200

    Homework 3_6 autotest for different languages

--- test_items.py
+++ test_items.py
@@ -1,8 +1,8 @@
 import time
 class TestLanguages():
-    def test_different_languages(self, browser):
+    def test_find_button(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
         time.sleep(30)
-        checkbasket = browser.find_element_by_css_selector("#add_to_basket_form")
-        assert checkbasket.is_displayed()
+        assert browser.find_element_by_css_selector("#add_to_basket_form"), \
+        'The button "add to basket" is not displayed'
